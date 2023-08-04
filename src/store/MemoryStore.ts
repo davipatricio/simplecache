b912f850b2c71235a6782aa6f03e51c17a35a661
Davi Patricio <davipatricio@pm.me>
2023-08-03 21:29:39 -0300

    refactor: use `Map` in `MemoryStore`

--- src/store/MemoryStore.ts
+++ src/store/MemoryStore.ts
@@ -2,31 +2,31 @@ import { setTimeout } from 'node:timers';
 import { BaseStore } from './BaseStore.js';
 
 export class MemoryStore extends BaseStore {
-  public data: Record<string, any> = {};
+  public data: Map<string, any> = new Map();
 
   public constructor(public defaultCacheTime?: number) {
     super();
   }
 
   public async get<T>(key: string): Promise<T> {
-    return this.data[key];
+    return this.data.get(key);
   }
 
   public async set<T>(key: string, value: T, expiresIn?: number) {
-    this.data[key] = value;
+    this.data.set(key, value);
 
     if (!expiresIn || !this.defaultCacheTime) return;
 
-    setTimeout(() => {
-      this.data[key] = undefined;
+    setTimeout(async () => {
+      await this.delete(key);
     }, expiresIn ?? this.defaultCacheTime).unref();
   }
 
   public async delete(key: string) {
-    this.data[key] = undefined;
+    this.data.delete(key);
   }
 
   public async clear() {
-    this.data = {};
+    this.data.clear();
   }
 }
