92f2683a20a23c12fe74dffac9c150c0a75353d4
Davi Patricio <davipatricio@pm.me>
2023-08-03 22:18:05 -0300

    fix caching time

--- src/store/MemoryStore.ts
+++ src/store/MemoryStore.ts
@@ -2,31 +2,42 @@ import { setTimeout } from 'node:timers';
 import { BaseStore } from './BaseStore.js';
 
 export class MemoryStore extends BaseStore {
-  public data: Map<string, any> = new Map();
+  private data: Map<string, any> = new Map();
 
   public constructor(public defaultCacheTime?: number) {
     super();
   }
 
-  public async get<T>(key: string): Promise<T> {
-    return this.data.get(key);
-  }
-
-  public async set<T>(key: string, value: T, expiresIn?: number) {
-    this.data.set(key, value);
-
-    if (!expiresIn || !this.defaultCacheTime) return;
-
-    setTimeout(async () => {
-      await this.delete(key);
-    }, expiresIn ?? this.defaultCacheTime).unref();
+  public async clear() {
+    this.data.clear();
   }
 
   public async delete(key: string) {
     this.data.delete(key);
   }
 
-  public async clear() {
-    this.data.clear();
+  public async get<T>(key: string): Promise<T> {
+    return this.data.get(key);
+  }
+
+  public async set<T>(
+    key: string,
+    value: T,
+    expiresIn = this.defaultCacheTime,
+  ) {
+    this.data.set(key, value);
+
+    if (
+      typeof this.defaultCacheTime !== 'number' ||
+      this.defaultCacheTime === 0 ||
+      typeof expiresIn !== 'number' ||
+      expiresIn === 0
+    ) {
+      return;
+    }
+
+    setTimeout(async () => {
+      await this.delete(key);
+    }, expiresIn).unref();
   }
 }
