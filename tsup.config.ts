ceab0b65a0ec10e180b12293be85f5e980048817
Davi Patricio <davipatricio@pm.me>
2023-08-03 22:18:42 -0300

    always generate d.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,9 +4,9 @@ export default defineConfig(options => ({
   entry: ['src/index.ts'],
   splitting: false,
   clean: true,
+  dts: true,
   format: ['cjs', 'esm'],
   target: 'node20',
   sourcemap: !options.watch,
-  dts: !options.watch,
   minify: !options.watch,
 }));
